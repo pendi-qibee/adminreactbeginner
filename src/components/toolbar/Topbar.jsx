dd347cc3da6beee657017ee91c0080b9d66ae956
pendimadyana <pendimadyana@gmail.com>
2021-10-24 21:49:41 +0700

    add mui material icon v5

--- src/components/toolbar/Topbar.jsx
+++ src/components/toolbar/Topbar.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import "./topbar.css"
+// import {NotificationsNone} from '@material-ui/icons';
+import {NotificationsNone} from '@mui/icons-material';
 
 export default function Topbar() {
     return (
         <div className="topbar">
             <div className="topbarWrapper">
-                <div className="topLeft"></div>
-                <div className="topRight"></div>
+                <div className="topLeft">
+                    <span className="logo">React Admin</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIcons">
+                       <NotificationsNone/>
+                    </div>
+                </div>
             </div>
         </div>
     )
