5fcf689b81e9b95480d14a17e775f1d40dca7a74
pendimadyana <pendimadyana@gmail.com>
2021-10-24 22:04:40 +0700

    add icon settings

--- src/components/toolbar/Topbar.jsx
+++ src/components/toolbar/Topbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./topbar.css"
 // import {NotificationsNone} from '@material-ui/icons';
-import {NotificationsNone} from '@mui/icons-material';
+import { NotificationsNone, Language, Settings } from '@mui/icons-material';
 
 export default function Topbar() {
     return (
@@ -12,8 +12,16 @@ export default function Topbar() {
                 </div>
                 <div className="topRight">
                     <div className="topbarIconContainer">
-                       <NotificationsNone/>
-                       <span className="topIconBadge">3</span>
+                        <NotificationsNone />
+                        <span className="topIconBadge">3</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings />
+                        <span className="topIconBadge">3</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Language />
+                        <span className="topIconBadge">3</span>
                     </div>
                 </div>
             </div>
