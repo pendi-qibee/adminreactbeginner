5e1325add9b1b45f2f8f7f2c3d92cee7adbc93be
pendimadyana <pendimadyana@gmail.com>
2021-10-26 16:14:42 +0700

    sidebar setup with icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Topbar from "./components/toolbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import "./app.css"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <Topbar/>
-    
+    <div className="container">
+      <Sidebar/>
+    <div className="others">other page</div>
+      
+    </div>
       </header>
     </div>
   );
