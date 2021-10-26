06f20230af4910db9afa4ef898acf3684f79fc9a
pendimadyana <pendimadyana@gmail.com>
2021-10-26 20:14:42 +0700

    add reusable chart

---
+++ src/dummyData.js
@@ -0,0 +1,51 @@
+export const userData = [
+    {
+        month: 'Jan',
+        "Active User": 1000,
+    },
+    {
+        month: 'Feb',
+        "Active User": 900,
+    },
+    {
+        month: 'Mar',
+        "Active User": 1200,
+    },
+    {
+        month: 'Apr',
+        "Active User": 2000,
+    },
+    {
+        month: 'May',
+        "Active User": 3000,
+    },
+    {
+        month: 'Jun',
+        "Active User": 4000,
+    },
+    {
+        month: 'Jul',
+        "Active User": 4500,
+    },
+    {
+        month: 'Aug',
+        "Active User": 4000,
+    },
+    {
+        month: 'Sep',
+        "Active User": 5000,
+    },
+    {
+        month: 'Oct',
+        "Active User": 4000,
+    },
+    {
+        month: 'Nov',
+        "Active User": 4000,
+    },
+    {
+        month: 'Des',
+        "Active User": 4000,
+    },
+
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
 import "./home.css"
 import FeaturedInfo from "../../components/featuredinfo/FeaturedInfo"
+import Chart from "../../components/chart/Chart"
+import { userData } from "../../dummyData";
+
+
 
 export default function Home() {
     return (
         <div className="home">
-            <FeaturedInfo/>
+            <FeaturedInfo />
+            <Chart 
+            data={userData} 
+            title="User Growth"
+            grid
+            dataKey="Active User" />
         </div>
     )
 }
